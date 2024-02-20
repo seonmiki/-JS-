74932df032b7e761b5e78d618725adf192bda082
seonmiki <seonmiki@student.42seoul.kr>
2024-02-20 20:40:32 +0900

    day 2

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi");
+const player = {
+	name: "miki",
+	points: 10,
+};
+
+let	num = 1;
+const num2 = 1;
+
+console.log(player);
+player.points = 15;
+player.add = "add";
+console.log(player);
+
+num = 2;
+num2 = 2;
