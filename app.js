5aeb17876fa5ccf2011e8a0a77d0cb3bdf18fa27
seonmiki <seonmiki@student.42seoul.kr>
2024-02-23 22:31:19 +0900

    day05

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
 const h1 = document.querySelector(".hello h1");
 
 function handletTitleClick(){
-	if ((event.button === 2) || (event.which === 3))
-	{
-		h1.innerText = "The mouse is here!";
-		h1.style.color = "purple";
-	}
+	h1.classList.toggle("clicked");
+	// const clickedClass = "cliked";
+	// if (h1.classList.contains(clickedClass))
+	// 	h1.classList.remove(clickedClass);
+	// else
+	// 	h1.classList.add(clickedClass);
 }
-
-function handleMouseEnter(){
-	h1.innerText = "The mouse is here!";
-	h1.style.color = "green";
-}
-
-function handleMouseLeave(){
-	h1.innerText = "The mouse is gone!";
-	h1.style.color = "skyblue";
-}
-
-function handleWindowResize(){
-	// document.body.style.backgroundColor = "tomato";
-	h1.innerText = "You just resized";
-	h1.style.color = "purple";
-}
-
-// function handleWindowCopy(){
-// 	alert("copier!");
-// }
-
-// function handleWindowOffline(){
-// 	alert("SOS no WIFI");
-// }
-
-// function handleWindowOnline(){
-// 	alert("ALL GOOD");
-// }
 
 h1.addEventListener("click", handletTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-// window.addEventListener("copy", handleWindowCopy);
-// window.addEventListener("offline", handleWindowOffline);
-// window.addEventListener("online", handleWindowOnline);
+function handleWindowResize() {
+	const ViewportWidth = window.innerWidth;
+  
+	if (ViewportWidth <= 500) {
+	  document.body.classList.toggle("small");
+	} else if (ViewportWidth <= 700) {
+	  document.body.classList.toggle("middle");
+	} else {
+	  document.body.classList.toggle("large");
+	}
+  }
+  
+  window.addEventListener("resize", handleWindowResize);
+  
