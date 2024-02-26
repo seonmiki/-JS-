34de1f553abd93e4d764d98a4f2167f0b6811c7a
seonmiki <seonmiki@student.42seoul.kr>
2024-02-26 22:27:15 +0900

    #4 event2: mousevent

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
+//first argument: information about the event that just happened
 function onSubmit(event)
 {
 	event.preventDefault(); //stop default event
 	console.log(event);
 }
+
+function handleLinkClick(event)
+{
+	event.preventDefault();
+	console.log(event);
+	// alert("clicked"); //막았다가 ok 누르면 실행됨
+}
 //onclick or pressEnter
 loginForm.addEventListener("submit", onSubmit);
+link.addEventListener("click", handleLinkClick);
