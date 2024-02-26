f10e37169878b42374352f18f462f1b8069000c5
seonmiki <seonmiki@student.42seoul.kr>
2024-02-26 22:18:22 +0900

    #4 event1

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
 
-function handletTitleClick(){
-	h1.classList.toggle("clicked");
-	// const clickedClass = "cliked";
-	// if (h1.classList.contains(clickedClass))
-	// 	h1.classList.remove(clickedClass);
-	// else
-	// 	h1.classList.add(clickedClass);
+function onSubmit(event)
+{
+	event.preventDefault(); //stop default event
+	console.log(event);
 }
-
-h1.addEventListener("click", handletTitleClick);
-
-function handleWindowResize() {
-	const ViewportWidth = window.innerWidth;
-  
-	if (ViewportWidth <= 500) {
-	  document.body.classList.toggle("small");
-	} else if (ViewportWidth <= 700) {
-	  document.body.classList.toggle("middle");
-	} else {
-	  document.body.classList.toggle("large");
-	}
-  }
-  
-  window.addEventListener("resize", handleWindowResize);
-  
+//onclick or pressEnter
+loginForm.addEventListener("submit", onSubmit);
