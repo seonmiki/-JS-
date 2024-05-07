1c1ab9dcad71afa384a8e53078e6d2b674d9ebd6
seonmin <seonmiki@student.42seoul.kr>
2024-05-07 19:42:35 +0900

    course fin

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "685f4decf1c9e16791f5c36b1a557129";
+
 function onGeoOk(position) {
 	const lat = position.coords.latitude;
 	const lng = position.coords.longitude;
-	console.log("You live in", lat, lng);
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+	fetch(url).then(response => response.json()).then(data => {
+		const weather = document.querySelector("#weather span:first-child");
+		const temp = document.querySelector("#weather span:nth-child(2)");
+		const city = document.querySelector("#weather span:last-child");
+		city.innerText = data.name;
+		temp.innerText = data.main.temp;
+		weather.innerText = data.weather[0].main;
+	});
 }
 
 function onGeoError() {
